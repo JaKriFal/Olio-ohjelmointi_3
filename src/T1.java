61410b2c89b06ce52c957a2e8c180a95254187c9
grummar <Jan.Falck@metropolia.fi>
2023-08-28 23:12:28 +0300

    t1 started

--- src/T1.java
+++ src/T1.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class T1 {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.print("This program asks you for coefficients for a quadratic equation ax^2 + bx + c = 0 and solves for real roots.");
+        System.out.println("This program asks you for coefficients for a quadratic equation ax^2 + bx + c = 0 and solves for real roots.");
         System.out.print("Give coefficient a:");
         double a = Double.parseDouble(reader.nextLine());
         System.out.print("Give coefficient b:");
